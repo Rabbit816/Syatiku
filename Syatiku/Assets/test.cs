1b887a2c13fc70d2f2227b5ef194380909d629f9
s <storm19980913@gmail.com>
2018-04-27 17:03:10 +0900

    飲み会のUI配置

---
+++ Syatiku/Assets/test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class test : MonoBehaviour {
+    
+    private GameObject obj;
+    private Vector2 aaa;
+	// Use this for initialization
+	void Start () {
+        obj = GameObject.Find("Image");
+        aaa = obj.transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //while(image.transform.localScale.x > 0) { 
+        aaa.x -= 0.5f * Time.deltaTime;
+        obj.transform.localScale = aaa;
+        //}
+    }
+}
