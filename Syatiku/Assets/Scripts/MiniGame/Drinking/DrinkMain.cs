33f9378a54ecc46c15164d2fadde0a0651879e52
hara kazunori <32507434+Rabbit816@users.noreply.github.com>
2018-05-11 17:00:39 +0900

    ランダムに吹き出しを表示非表示させる

--- Syatiku/Assets/Scripts/MiniGame/Drinking/DrinkMain.cs
+++ Syatiku/Assets/Scripts/MiniGame/Drinking/DrinkMain.cs
@@ -6,29 +6,44 @@ using System.Linq;//シャッフルする時に必要
 using System;
 
 public class DrinkMain : MonoBehaviour {
-    public Image[] tnm;
-    public int[] num;
+    [SerializeField]
+    private Image[] tnm;
+    private int[] num;
+    private int[] num2;
     private float timer;
     private int count;
     private int index;
     private bool open;
-
+    private bool moyamoya;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         open = false;
+        moyamoya = false;
         //numの初期化       
-        num = new int[4] { 0, 1, 2, 3 };
+        num = new int[4] { 0, 1, 2, 3 };        
         //tnmの初期化
-        for(int i = 0; i < tnm.Length; ++i)
+        for (int i = 0; i < tnm.Length; ++i)
         {
             tnm[i].enabled = false;
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {      
+        transform.parent = GameObject.Find("tnm").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
         //一秒毎にする
+        if (!moyamoya)
+        {
+            fromleft();
+            //randomtnm();
+        }        
+    }
+   
+    public void fromleft()
+    {       
         timer += Time.deltaTime;
         if (timer > 1)
         {
@@ -51,8 +66,37 @@ public class DrinkMain : MonoBehaviour {
         }
         if (count == 4)
         {
-            count = 0;
+            moyamoya = true;
         }
+    }
 
+    public void randomtnm()
+    {
+        num2 = num.OrderBy(i => Guid.NewGuid()).ToArray();
+        timer += Time.deltaTime;
+        if (timer > 1)
+        {
+            if (open)
+            {
+                open = false;
+                tnm[index].enabled = false;
+                count += 1;
+                timer = 0;
+                Debug.Log("2秒経過");
+            }
+            else
+            {
+                timer = 0;
+                open = true;
+                index = Array.IndexOf(num2, count);
+                tnm[index].enabled = true;
+                Debug.Log("1秒経過");
+            }
+        }
+        if (count == 4)
+        {
+            moyamoya = true;
+        }
     }
 }
+
---
+++ Syatiku/Assets/Scripts/MiniGame/Drinking/ryouri.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using System;
+
+
+public class ryouri : MonoBehaviour {
+    [SerializeField]
+    private Image[] tnm;    
+    private int index;
+    private bool open;
+    private int[] num;
+    private int[] num2;
+    private float timer;
+
+    // Use this for initialization
+    void Start () {
+        num = new int[4] { 0, 1, 2, 3 };
+        num2 = num.OrderBy(i => Guid.NewGuid()).ToArray();
+        for (int i = 0; i < tnm.Length; ++i)
+        {
+            tnm[i].enabled = false;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if(open == true)
+        {
+            if (timer > 1)
+            {
+                open = false;
+                tnm[index].enabled = false;          
+                Debug.Log("料理表示");
+            }
+            else
+            {
+                open = true;
+                index = Array.IndexOf(num2, 0);
+                tnm[index].enabled = true;
+                Debug.Log("料理非表示");
+            }
+        }
+        
+	}
+}
