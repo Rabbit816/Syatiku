52932592e611132ab787dd4d3c9af4fb6f982628
blacky0424 <nuhs_ihcugikes@yahoo.co.jp>
2018-05-15 10:37:31 +0900

    コミット

--- Syatiku/Assets/Scripts/MiniGame/Hacking/HackMain.cs
+++ Syatiku/Assets/Scripts/MiniGame/Hacking/HackMain.cs
@@ -3,24 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HackMain : MonoBehaviour {
+public class HackMain : Common {
 
     [SerializeField,Tooltip("時間制限初期値")]
     private float timer = 30.0f;
 
-    [SerializeField]
+    [SerializeField,Tooltip("時間オブジェクト")]
     private Text time;
 
+    //お題のアニメーションが終わったかどうか
     bool _themeActive = false;
 
-    [SerializeField]
+    [SerializeField,Tooltip("お題の名前")]
     private string[] _chipped;
 
-    [SerializeField]
+    [SerializeField,Tooltip("お題のオブジェクト")]
     private GameObject theme_obj;
-
-    [HideInInspector]       //publicで宣言してもInspectorからは見えない
-    public int i = 10;
 
 	// Use this for initialization
 	void Start () {
@@ -33,8 +31,6 @@ public class HackMain : MonoBehaviour {
         if(_themeActive)
             if (Timer())
                 Timer();
-            else
-                Scene();
     }
 
     /// <summary>
@@ -45,8 +41,12 @@ public class HackMain : MonoBehaviour {
     {
         timer -= Time.deltaTime;
         time.text = "Timer: " + timer.ToString("f1");
-        bool _time = timer <= 0 ? true : false;
-        return _time;
+        if(timer < 0f)
+        {
+            time.text = "Timer: 0.0";
+            ChangeScene(SceneName.Title);
+        }
+        return (timer < 0f);
     }
 
     /// <summary>
@@ -57,18 +57,16 @@ public class HackMain : MonoBehaviour {
         //お題の内容ランダムで選択し出現（移動）
         int rand_theme = Random.Range(0, _chipped.Length-1);
         Debug.Log("お題 : " + rand_theme + ":" + _chipped[rand_theme]);
+
         theme_obj.GetComponentInChildren<Text>(true).text = _chipped[rand_theme].ToString();
         Animation anim = theme_obj.GetComponent<Animation>();
         anim.Play();
-        if (!anim.isPlaying)
+        if (anim.Play())
+            _themeActive = false;
+        else
             _themeActive = true;
 
         //出現中何もできないようにする
 
     }
-
-    /// <summary>
-    /// あとでわっしーがシーン移動関数を作るのでそれを使う
-    /// </summary>
-    void Scene() { }
 }
--- Syatiku/Assets/Scripts/MiniGame/Hacking/HackTap.cs
+++ Syatiku/Assets/Scripts/MiniGame/Hacking/HackTap.cs
@@ -2,15 +2,101 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HackTap : MonoBehaviour {
+public class HackTap : MonoBehaviour
+{
+    private struct PlaceList
+    {
+        public GameObject pos;
+        public string word;
+    };
+    //これにランダムで選ばれた場所に単語を格納していく
+    private PlaceList[] place_list = new PlaceList[]
+    {
+        new PlaceList(){ pos=null, word = "" },
+    };
 
+    [SerializeField, Tooltip("全部のタップできる場所格納")]
+    private GameObject[] place;
+
+    [SerializeField, Tooltip("全部の発見できる単語")]
+    private string[] str;
+
+    private HashSet<int> rand_list = new HashSet<int>();
+    private int rand;
+
+    //現在の場所
+    private int place_current;
+
+    //単語が表示されているかどうか
+    private bool _placeAnim = false;
+
+    [SerializeField,Tooltip("単語が表示される時間")]
+    private float t = 5.0f;
+
+    [SerializeField,Tooltip("出す単語画像")]
+    private GameObject Prefab;
+    
 	// Use this for initialization
 	void Start () {
-		
+        _placeAnim = false;
+        AddPlaceWord();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (_placeAnim)
+        {
+            t -= Time.deltaTime;
+            if (t <= 0.0f)
+            {
+                place[place_current].transform.GetChild(0).gameObject.SetActive(false);
+                _placeAnim = false;
+            }
+        }
 	}
+
+    /// <summary>
+    /// タップしたところから単語が出てくる処理
+    /// </summary>
+    /// <param name="placeNum">どの場所かを指定</param>
+    public void PlaceButton(int placeNum){
+        // 一回もタップされてなかったら
+        if(place[placeNum].transform.childCount == 0)
+        {
+            place_list[placeNum].word.ToString();
+            GameObject _prefab = Instantiate(Prefab);
+            _prefab.transform.SetParent(place[placeNum].transform, false);  // SetParent(親の場所,大きさを変えるか)
+            _placeAnim = true;
+        }
+        place_current = placeNum;
+    }
+
+    /// <summary>
+    /// ランダムで単語を選ぶ処理
+    /// </summary>
+    /// <returns></returns>
+    private string HackRandom() {
+        if (rand_list.Count > str.Length)
+            rand_list.Clear();
+        //randが重複したら回す
+        while (!rand_list.Add(rand))
+        {
+            rand = Random.Range(0, str.Length);
+            rand_list.Add(rand);
+            Debug.Log("rand:" + rand);
+        }
+        return str[rand];
+    }
+
+    /// <summary>
+    /// 各場所に単語を入れる
+    /// </summary>
+    private void AddPlaceWord()
+    {
+        for (int i=0; i<place.Length; i++)
+        {
+            place_list[i].word = HackRandom();
+            place_list[i].pos = place[i];
+        }
+    }
 }
