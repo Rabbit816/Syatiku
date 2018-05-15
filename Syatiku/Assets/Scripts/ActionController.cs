da770ab9bb907ffa1ade7b243a51836978e4330f
sato-kazuki <himazin19970630@gmail.com>
2018-05-15 18:38:37 +0900

    画面遷移処理を変更

--- Syatiku/Assets/Scripts/ActionController.cs
+++ Syatiku/Assets/Scripts/ActionController.cs
@@ -13,21 +13,21 @@ public class ActionController : MonoBehaviour {
 		
 	}
 
-    //public void ChangeMinigame(int num)
-    //{
-    //    switch (num)
-    //    {
-    //        case 0:
-    //            ChangeScene(SceneName.Smoking);
-    //            break;
-    //        case 1:
-    //            ChangeScene(SceneName.Hacking);
-    //            break;
-    //        case 2:
-    //            ChangeScene(SceneName.Drinking);
-    //            break;
-    //        default:
-    //            break;
-    //    }
-    //}
+    public void ChangeMinigame(int num)
+    {
+        switch (num)
+        {
+            case 0:
+                Common.Instance.FadeChangeScene(Common.SceneName.Smoking, 1.0f);
+                break;
+            case 1:
+                Common.Instance.FadeChangeScene(Common.SceneName.Hacking, 1.0f);
+                break;
+            case 2:
+                Common.Instance.FadeChangeScene(Common.SceneName.Drinking, 1.0f);
+                break;
+            default:
+                break;
+        }
+    }
 }
