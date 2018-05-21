887cca540404e29379d85c28e7630fd8f24344cf
paseso <16d3170001ae@edu.tech.ac.jp>
2018-05-21 20:30:15 +0900

    Hacking PC内処理追加

--- Syatiku/Assets/Scripts/MiniGame/Hacking/HackPC.cs
+++ Syatiku/Assets/Scripts/MiniGame/Hacking/HackPC.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public class HackPC : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    private float _drag = 0.0f;
+    private float _dorp = 0.0f;
+    private bool _dragActive = false;
+    private Vector3 dragVec;
+    private Vector3 screenToWorldPointPosition;
+    private Vector3 pointer;
+    private GameObject wordbtn;
+    private Transform trans;
+
+    // Use this for initialization
+    void Start () {
+        _dragActive = false;
+        wordbtn = GameObject.Find("Prefabs/MiniGame/WordImage");
+        //Button btn = wordbtn.GetComponent<Button>().OnClick(OnTouchDrag(trans));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        
 	}
 
     /// <summary>
@@ -39,5 +47,15 @@ public class HackPC : MonoBehaviour {
         // 文字列があっているかどうか処理
 
         return true;
+    }
+
+    private void OnTouchDrag(Transform a)
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            dragVec = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+            screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(dragVec);
+            a.transform.position = screenToWorldPointPosition;
+        }
     }
 }
