1d523cedfb5f71f96aeb7f20261da95301c55b27
unknown <16d3170001ae@edu.tech.ac.jp>
2018-05-29 11:45:15 +0900

    Hacking ドロップ処理追加

--- Syatiku/Assets/Scripts/MiniGame/Hacking/Drag.cs
+++ Syatiku/Assets/Scripts/MiniGame/Hacking/Drag.cs
@@ -1,30 +1,22 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
-
 using System.Collections.Generic;
 
-public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler,IPointerEnterHandler
+public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    private Vector3 startR;
-    private RectTransform rect;
-
     private Ray ray;
     private RaycastHit2D hit;
-    int rayLayer = 0;
-
+    private GameObject collect;
     Vector3 dragVec;
-    Vector3 old_vec;
 
     void Start()
     {
-        rect = GetComponent<RectTransform>();
-        startR = rect.localPosition;
-        rayLayer =  8;
+        collect = GameObject.Find("Canvas/IntoPC/CollectedWord");
     }
 
     private void Update()
     {
-        
+
     }
 
     public void OnBeginDrag(PointerEventData pointer)
@@ -34,79 +26,40 @@ public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHand
 
     public void OnDrag(PointerEventData pointer)
     {
+        RaycastResult result = pointer.pointerPressRaycast;
+        Debug.Log(gameObject.transform.parent.tag);
+        if (gameObject.transform.transform.tag  == "asnwer")
+        {
+            Debug.Log("おとんみっけ");
+            transform.SetParent(collect.transform, false);
+        }
+
         dragVec = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5);
         gameObject.transform.position = Camera.main.ScreenToWorldPoint(dragVec);
     }
 
     public void OnEndDrag(PointerEventData pointer)
     {
-        //ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        //Vector3 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //Debug.Log("EndDrag");
-        //hit = new RaycastHit2D();
-        //if (Physics2D.Raycast(ray.origin, new Vector3(0,0,-30), Mathf.Infinity))
-        //{
-        //    if(hit.collider.tag == "answer")
-        //    {
-        //        Debug.Log("とれてるよ");
-        //    }
-        //    Debug.Log(hit.collider.gameObject.name);
-        //    gameObject.transform.SetParent(hit.transform.transform);
-        //}
+        Debug.Log("とれてるよ");
 
-        //if (hit)
-        //{
-        //    Debug.Log("OnEndDrag");
-        //    if (hit.collider.tag == "answer")
-        //    {
-        //        gameObject.transform.parent = hit.collider.transform.transform;
-        //        //gameObject.transform.position = hit.collider.GetComponent<Transform>().position;
-        //    }
-        //    else
-        //    {
-        //        rect.localPosition = startR;
-        //    }
-        //}
-        //old_vec = vec;
-        if (hit.collider)
-        {
-            Debug.Log("きるよ～" + hit.collider.tag.ToString());
-        }
-    }
-
-    public void OnDrop(PointerEventData pointer)
-    {
         var raycastResults = new List<RaycastResult>();
         EventSystem.current.RaycastAll(pointer, raycastResults);
-        Debug.Log("はいってまっせ");
         foreach (var hit in raycastResults)
         {
-            Debug.Log("foreachの中");
             // もし answer の上なら、その位置に固定する
             if (hit.gameObject.CompareTag("answer"))
             {
-                Debug.Log("answer発見");
-                transform.SetParent(hit.gameObject.transform, false);
-                this.enabled = false;
+                if (hit.gameObject.transform.childCount == 0)
+                {
+                    transform.SetParent(hit.gameObject.transform, false);
+                    transform.localPosition = Vector2.zero;
+                }
+                else
+                {
+                    return;
+                }
+                
             }
-            else
-            {
-                rect.localPosition = startR;
-            }
-        }
-    }
-
-    public void OnPointerEnter(PointerEventData pointer)
-    {
-        if (Input.GetMouseButtonUp(0))
-        {
-            Debug.Log("きてるぅ");
-            //gameObject.transform.parent = hit.collider.transform.transform;
-        }
-
-        if (pointer.pointerDrag == null)
-        {
-            return;
         }
     }
 }
