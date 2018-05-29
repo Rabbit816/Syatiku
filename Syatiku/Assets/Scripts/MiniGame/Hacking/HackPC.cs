a09eb76019631e949156a637661beabccfe90666
yatao0526 <yuuki0523yata@gmail.com>
2018-05-29 16:24:39 +0900

    Hacking コミット

--- Syatiku/Assets/Scripts/MiniGame/Hacking/HackPC.cs
+++ Syatiku/Assets/Scripts/MiniGame/Hacking/HackPC.cs
@@ -8,16 +8,19 @@ public class HackPC : MonoBehaviour {
     private Text prefabText;
     [HideInInspector]
     public int counter = 0;
+    private GameObject collect;
 
     // Use this for initialization
     void Start () {
+        collect = GameObject.Find("Canvas/IntoPC/CollectedWord");
         ChippedString();
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
         
-	}
+    }
 
     /// <summary>
     /// 欠けてる文章の処理
@@ -37,34 +40,35 @@ public class HackPC : MonoBehaviour {
 
     }
 
-    public bool CheckString()
+    public void CheckString()
     {
         bool _isCheck = false;
-        for(int i=0; i<6; i++)
-        {
-            GameObject ans = GameObject.Find("Canvas/IntoPC/Answer/AnswerText_" + i);
-            GameObject quest = GameObject.Find("Canvas/IntoPC/Quest/QuestText_" + i);
-            Text que_text = quest.GetComponent<Text>();
-            Transform ans_child = ans.transform.GetChild(0).GetChild(0);
-            Text ansChild_text = GetComponent<Text>();
-            if (ansChild_text.text.Substring(0,que_text.text.Length) == que_text.text)
+        
+            for (int i = 0; i <= 5; i++)
             {
-                _isCheck = true;
-                if(i == 5)
+                GameObject ans = GameObject.Find("Canvas/IntoPC/Answer/AnswerText_" + i);
+                GameObject quest = GameObject.Find("Canvas/IntoPC/Quest/QuestText_" + i);
+                Text que_text = quest.GetComponent<Text>();
+                Transform ans_child = ans.transform.GetChild(0).GetChild(0);
+                Text ansChild_text = ans_child.GetComponent<Text>();
+                if (ansChild_text.text.Substring(0, ansChild_text.text.Length) == que_text.text)
                 {
-                    //Common.Instance.gameScore("Hacking",100);
-                    Common.Instance.ChangeScene(Common.SceneName.Result);
+                    _isCheck = true;
+                    if (i == 5)
+                    {
+                        //Common.Instance.gameScore("Hacking",100);
+                        Common.Instance.ChangeScene(Common.SceneName.Result);
+                    }
                 }
-            }
-            else
-            {
-                _isCheck = false;
-                break;
-            }
+                else
+                {
+                    _isCheck = false;
+                    break;
+                }
         }
+        
         // 文字列があっているかどうか処理
         counter++;
-        return true;
     }
     
 }
