b999354b0532a1b6825dff4c20c77b6b0d3a729f
yatao0526 <yuuki0523yata@gmail.com>
2018-05-29 16:28:16 +0900

    Revert "Hacking コミット"
    This reverts commit a09eb76019631e949156a637661beabccfe90666.

--- Syatiku/Assets/Scripts/MiniGame/Hacking/Drag.cs
+++ Syatiku/Assets/Scripts/MiniGame/Hacking/Drag.cs
@@ -1,22 +1,30 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+
 using System.Collections.Generic;
 
-public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler,IPointerEnterHandler
 {
+    private Vector3 startR;
+    private RectTransform rect;
+
     private Ray ray;
     private RaycastHit2D hit;
-    private GameObject collect;
+    int rayLayer = 0;
+
     Vector3 dragVec;
+    Vector3 old_vec;
 
     void Start()
     {
-        collect = GameObject.Find("Canvas/IntoPC/CollectedWord");
+        rect = GetComponent<RectTransform>();
+        startR = rect.localPosition;
+        rayLayer =  8;
     }
 
     private void Update()
     {
-
+        
     }
 
     public void OnBeginDrag(PointerEventData pointer)
@@ -26,40 +34,79 @@ public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHand
 
     public void OnDrag(PointerEventData pointer)
     {
-        RaycastResult result = pointer.pointerPressRaycast;
-        Debug.Log(gameObject.transform.parent.tag);
-        if (gameObject.transform.transform.tag  == "asnwer")
-        {
-            Debug.Log("おとんみっけ");
-            transform.SetParent(collect.transform, false);
-        }
-
         dragVec = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5);
         gameObject.transform.position = Camera.main.ScreenToWorldPoint(dragVec);
     }
 
     public void OnEndDrag(PointerEventData pointer)
     {
-        Debug.Log("とれてるよ");
+        //ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //Vector3 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Debug.Log("EndDrag");
+        //hit = new RaycastHit2D();
+        //if (Physics2D.Raycast(ray.origin, new Vector3(0,0,-30), Mathf.Infinity))
+        //{
+        //    if(hit.collider.tag == "answer")
+        //    {
+        //        Debug.Log("とれてるよ");
+        //    }
+        //    Debug.Log(hit.collider.gameObject.name);
+        //    gameObject.transform.SetParent(hit.transform.transform);
+        //}
 
+        //if (hit)
+        //{
+        //    Debug.Log("OnEndDrag");
+        //    if (hit.collider.tag == "answer")
+        //    {
+        //        gameObject.transform.parent = hit.collider.transform.transform;
+        //        //gameObject.transform.position = hit.collider.GetComponent<Transform>().position;
+        //    }
+        //    else
+        //    {
+        //        rect.localPosition = startR;
+        //    }
+        //}
+        //old_vec = vec;
+        if (hit.collider)
+        {
+            Debug.Log("きるよ～" + hit.collider.tag.ToString());
+        }
+    }
+
+    public void OnDrop(PointerEventData pointer)
+    {
         var raycastResults = new List<RaycastResult>();
         EventSystem.current.RaycastAll(pointer, raycastResults);
+        Debug.Log("はいってまっせ");
         foreach (var hit in raycastResults)
         {
+            Debug.Log("foreachの中");
             // もし answer の上なら、その位置に固定する
             if (hit.gameObject.CompareTag("answer"))
             {
-                if (hit.gameObject.transform.childCount == 0)
-                {
-                    transform.SetParent(hit.gameObject.transform, false);
-                    transform.localPosition = Vector2.zero;
-                }
-                else
-                {
-                    return;
-                }
-                
+                Debug.Log("answer発見");
+                transform.SetParent(hit.gameObject.transform, false);
+                this.enabled = false;
             }
+            else
+            {
+                rect.localPosition = startR;
+            }
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData pointer)
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("きてるぅ");
+            //gameObject.transform.parent = hit.collider.transform.transform;
+        }
+
+        if (pointer.pointerDrag == null)
+        {
+            return;
         }
     }
 }
--- Syatiku/Assets/Scripts/MiniGame/Hacking/HackPC.cs
+++ Syatiku/Assets/Scripts/MiniGame/Hacking/HackPC.cs
@@ -8,19 +8,16 @@ public class HackPC : MonoBehaviour {
     private Text prefabText;
     [HideInInspector]
     public int counter = 0;
-    private GameObject collect;
 
     // Use this for initialization
     void Start () {
-        collect = GameObject.Find("Canvas/IntoPC/CollectedWord");
         ChippedString();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update () {
         
-    }
+	}
 
     /// <summary>
     /// 欠けてる文章の処理
@@ -40,35 +37,34 @@ public class HackPC : MonoBehaviour {
 
     }
 
-    public void CheckString()
+    public bool CheckString()
     {
         bool _isCheck = false;
-        
-            for (int i = 0; i <= 5; i++)
+        for(int i=0; i<6; i++)
+        {
+            GameObject ans = GameObject.Find("Canvas/IntoPC/Answer/AnswerText_" + i);
+            GameObject quest = GameObject.Find("Canvas/IntoPC/Quest/QuestText_" + i);
+            Text que_text = quest.GetComponent<Text>();
+            Transform ans_child = ans.transform.GetChild(0).GetChild(0);
+            Text ansChild_text = GetComponent<Text>();
+            if (ansChild_text.text.Substring(0,que_text.text.Length) == que_text.text)
             {
-                GameObject ans = GameObject.Find("Canvas/IntoPC/Answer/AnswerText_" + i);
-                GameObject quest = GameObject.Find("Canvas/IntoPC/Quest/QuestText_" + i);
-                Text que_text = quest.GetComponent<Text>();
-                Transform ans_child = ans.transform.GetChild(0).GetChild(0);
-                Text ansChild_text = ans_child.GetComponent<Text>();
-                if (ansChild_text.text.Substring(0, ansChild_text.text.Length) == que_text.text)
+                _isCheck = true;
+                if(i == 5)
                 {
-                    _isCheck = true;
-                    if (i == 5)
-                    {
-                        //Common.Instance.gameScore("Hacking",100);
-                        Common.Instance.ChangeScene(Common.SceneName.Result);
-                    }
+                    //Common.Instance.gameScore("Hacking",100);
+                    Common.Instance.ChangeScene(Common.SceneName.Result);
                 }
-                else
-                {
-                    _isCheck = false;
-                    break;
-                }
+            }
+            else
+            {
+                _isCheck = false;
+                break;
+            }
         }
-        
         // 文字列があっているかどうか処理
         counter++;
+        return true;
     }
     
 }
