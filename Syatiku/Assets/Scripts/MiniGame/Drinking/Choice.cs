6670534aff17512f9c0bd21658d6318cda3526c1
natuto1998 <natuto1998@users.noreply.github.com>
2018-06-04 12:02:50 +0900

    特に変更なし

--- Syatiku/Assets/Scripts/MiniGame/Drinking/Choice.cs
+++ Syatiku/Assets/Scripts/MiniGame/Drinking/Choice.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 public class Choice : MonoBehaviour {    
 
     [SerializeField]
-    private Sprite[] menu;
+    private Image[] menu;
     [SerializeField]
     private Image[] niku;
     [SerializeField]
@@ -19,12 +19,13 @@ public class Choice : MonoBehaviour {
     void Start () {
         //画像を見えなくする
         for (int i = 0; i < sake.Length; ++i)
-        {
+        {            
             niku[i].enabled = false;
             sake[i].enabled = false;
             sashimi[i].enabled = false;
             salad[i].enabled = false;            
-        }       
+        }
+        //gameObject.SetActive(false);
     }
 	
 	void Update () {
@@ -35,22 +36,23 @@ public class Choice : MonoBehaviour {
         //ボタンを押してAnswerに表示
         if (niku[i].enabled == false)
         {
-            
+            niku[0] = menu[0];
+            niku[i].enabled = true;            
             Debug.Log("肉");
         }
         else if (sake[i].enabled == false)
         {
-            
+            sake[i].enabled = true;
             Debug.Log("酒");
         }
         else if (sashimi[i].enabled == false)
         {
-            
+            sashimi[i].enabled = true;
             Debug.Log("刺身");
         }
         else if (salad[i].enabled == false)
         {
-            
+            salad[i].enabled = true;
             Debug.Log("サラダ");
         }            
     }
