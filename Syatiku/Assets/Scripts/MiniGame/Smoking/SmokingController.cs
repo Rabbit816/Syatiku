f1b5c54b1d36b0167cc75cc4570c975461146f02
mamanju <himazin19970630@gmail.com>
2018-06-05 13:57:31 +0900

    喫煙を少し変更

--- Syatiku/Assets/Scripts/MiniGame/Smoking/SmokingController.cs
+++ Syatiku/Assets/Scripts/MiniGame/Smoking/SmokingController.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SmokingController : MonoBehaviour {
-
+    [SerializeField]
+    private Image tabaco;
 	// Use this for initialization
 	void Start () {
-		
+        StartCoroutine(TimeDown());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public IEnumerator TimeDown()
+    {
+        while (tabaco.rectTransform.sizeDelta.x > 0)
+        {
+            tabaco.rectTransform.sizeDelta -= new Vector2(10,0);
+            yield return null;
+        }
+    }
 }
