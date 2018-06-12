24338ef45cc4dd80d660ba4bc3e49dff8b3bfa08
mamanju <himazin19970630@gmail.com>
2018-06-12 19:09:36 +0900

    ビルドデータ追加

---
+++ Syatiku/Assets/Scripts/CSV/CSVLoad.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class CSVLoad : MonoBehaviour {
+    private string filePath = "CSV/CSVTest.csv";
+    private string[] textList;
+    private TextAsset fileData;
+	// Use this for initialization
+	void Start () {
+        
+        fileData = Resources.Load("CSV/CSVTest") as TextAsset;
+        //foreach(var i in )
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
