205e39aa85dd553ffebd3fddefd387aa6376f589
paseso <16d3170001ae@edu.tech.ac.jp>
2018-06-15 09:25:22 +0900

    Revert "Hacking読み込みに修正"
    This reverts commit 2182438b7ef990c730f3db479ff2cefbf8feba3d.

--- Syatiku/Assets/Scripts/MiniGame/Hacking/HackMain.cs
+++ Syatiku/Assets/Scripts/MiniGame/Hacking/HackMain.cs
@@ -25,7 +25,9 @@ public class HackMain : MonoBehaviour {
     private string[] res;
 
     private string current;
-    
+    private int line = 0;
+    [HideInInspector]
+    public List<string[]> str_list = new List<string[]>();
 
     // Use this for initialization
     void Start () {
@@ -67,12 +69,25 @@ public class HackMain : MonoBehaviour {
         if(timer < 0f)
         {
             time.text = "Timer: 0.0";
-            Common.Instance.ChangeScene(Common.SceneName.Result);
+            //ChangeScene(SceneName.Title);
         }
         return (timer < 0f);
     }
 
-    
+    private void ReadText()
+    {
+        TextAsset csvfile = Resources.Load("Hacking/Quest") as TextAsset;
+        System.IO.StringReader stren = new System.IO.StringReader(csvfile.text);
+        Debug.Log("text: " + csvfile.ToString());
+        // 表示
+        while (stren.Peek() > -1)
+        {
+            string str = stren.ReadLine();
+            str_list.Add(str.Split(',')); // リストに入れる
+
+            line++; // 行数加算
+        }
+    }
     /// <summary>
     /// お題の処理
     /// </summary>
