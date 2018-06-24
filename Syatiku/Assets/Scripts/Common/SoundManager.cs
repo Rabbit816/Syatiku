e2526540d9f63acaf75ab2f44ee40b59bfb34f81
blacky0424 <nuhs_ihcugikes@yahoo.co.jp>
2018-06-25 01:13:42 +0900

    適当にサウンドマネージャー

---
+++ Syatiku/Assets/Scripts/Common/SoundManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    static SoundManager instance;
+    public static SoundManager Instance {
+        get
+        {
+            if (instance != null) return instance;
+            instance = FindObjectOfType<SoundManager>();
+            return instance;
+        }
+    }
+
+    [SerializeField]
+    CriAtomSource bgmSource;
+    [SerializeField]
+    CriAtomSource seSource;
+    [SerializeField]
+    CriAtomSource voiceSource;
+
+    void Start()
+    {
+        //bgmSource.Play("futta-rainbow");
+        DontDestroyOnLoad(this);
+    }
+
+    public void PlayBGM(string cueName)
+    {
+        bgmSource.Play(cueName);
+    }
+
+    public void PlaySE(string cueName)
+    {
+        seSource.Play(cueName);
+    }
+
+    public void PlayVoice(string cueName)
+    {
+        voiceSource.Play(cueName);
+    }
+
+}
