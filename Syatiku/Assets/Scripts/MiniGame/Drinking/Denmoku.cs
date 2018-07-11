e801b45278f9c4d2e3f37d99d80e42960074d9ba
HaraKazunori <17c1020021@edu.tech.ac.jp>
2018-07-11 16:20:22 +0900

    ボタンの追加・スクリプト修正

---
+++ Syatiku/Assets/Scripts/MiniGame/Drinking/Denmoku.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Denmoku : MonoBehaviour {
+
+    public Slider InfoMeter;
+
+    //メニューリスト
+    public Image Yakitori;
+    public Image Sake;
+    public Image Salad;
+    public Image Sashimi;
+
+    //デンモク内で使う配列・変数
+    private int[] InputOrderBox = new int[4];
+    private int[] InputOrderCounter = new int[4];
+    private float[] OrderListPos = new float[4] {-170, -265, -360, -455};
+    private int DenmokuCounter = 0;
+    
+
+	void Start () {
+        
+	}
+	
+	void Update () {
+		
+	}
+}
--- Syatiku/Assets/Scripts/MiniGame/Drinking/DrinkScene.cs
+++ Syatiku/Assets/Scripts/MiniGame/Drinking/DrinkScene.cs
@@ -7,27 +7,37 @@ public class DrinkScene : MonoBehaviour {
 
     public GameObject menuObject;
     public GameObject hukidashiObj;
+    public Text OrderCounter1;
+    public Text OrderCounter2;
+    public Text OrderCounter3;
+    public Text OrderCounter4;
+
     public GameObject yakitoriObj;
     public GameObject sakeObj;
     public GameObject saladObj;
     public GameObject sashimiObj;
 
-    //注文を表示するための配列
-    public int[] foodsBox = new int[4];
-    public float[] orderPos = new float[4] {-6.5f, -2, 2.3f, 6.5f};
+    //商品を格納する配列
+    private int[] foodsBox = new int[4];
 
-    //配列の中身をシャッフルする
+    //注文を表示するための配列
+    public int[] OrderBox = new int[4];
+    public int[] OrderCounter = new int[4];
+    public float[] OrderPos = new float[4] {-6.37f, -2.12f, 2.12f, 6.37f};
+    System.Random rnd = new System.Random();
+
+    private int[] Num = new int[4];
+    private int NumCounter = 0;
+
+    //注文の配列の用意
     public void OrderShuffle()
     {
-        //配列の中身を用意
-        for (int i = 0; i < foodsBox.Length; i++)
+        //商品を格納する配列を用意&シャッフル
+        for(int i = 0; i < foodsBox.Length; i++)
         {
-            foodsBox[i] = Random.Range(0, foodsBox.Length + 1);
+            foodsBox[i] = i;
         }
-
-        //配列をシャッフル
-        System.Random rnd = new System.Random();
-        for (int i = 0; i < foodsBox.Length; i++)
+        for(int i = 0; i < foodsBox.Length; i++)
         {
             int shuffle1 = rnd.Next(foodsBox.Length);
             int shuffle2 = rnd.Next(foodsBox.Length);
@@ -35,85 +45,92 @@ public class DrinkScene : MonoBehaviour {
             foodsBox[shuffle1] = foodsBox[shuffle2];
             foodsBox[shuffle2] = value;
         }
-
-        for (int i = 0; i < orderPos.Length; i++)
+        
+        //注文配列・個数配列・Num配列の用意
+        for(int i = 0; i < OrderBox.Length; i++)
         {
-            int shuffle1 = rnd.Next(orderPos.Length);
-            int shuffle2 = rnd.Next(orderPos.Length);
-            float value = orderPos[shuffle1];
-            orderPos[shuffle1] = orderPos[shuffle2];
-            orderPos[shuffle2] = value;
+            OrderBox[i] = foodsBox[i];
+            OrderCounter[i] = Random.Range(1, 5);
+            Num[i] = i;
         }
-
-
     }
     
-    //注文商品を1個ずつランダムに表示して消すを繰り返す
+    //表示する位置をシャッフル
+    public void PosShuffle()
+    {
+        for (int i = 0; i < OrderPos.Length; i++)
+        {
+            int shuffle1 = rnd.Next(OrderPos.Length);
+            int shuffle2 = rnd.Next(OrderPos.Length);
+            float value = OrderPos[shuffle1];
+            OrderPos[shuffle1] = OrderPos[shuffle2];
+            OrderPos[shuffle2] = value;
+
+            //Num配列のシャッフル
+            int val = Num[shuffle1];
+            Num[shuffle1] = Num[shuffle2];
+            Num[shuffle2] = val;
+        }
+    }
+
+    //注文商品を1個ずつランダムな位置に表示して消すを繰り返す
     private IEnumerator OrderMethod()
     {
-        yield return new WaitForSeconds(1.5f);
-        for (int i = 0; i < foodsBox.Length; i++)
+        for (int i = 0; i < OrderBox.Length; i++)
         {
-            switch (foodsBox[i])
+            yield return new WaitForSeconds(1.0f);
+            //吹き出しを表示
+            var hukidashi = Instantiate(hukidashiObj, new Vector2(OrderPos[i], 3.3f), Quaternion.identity);
+            hukidashi.transform.localScale = new Vector2(1.25f, 1.25f);
+            hukidashi.transform.parent = menuObject.transform;
+
+            //注文数の表示
+            OrderNum();
+
+            switch (OrderBox[i])
             {
+                //やきとりを表示
                 case 0:
-                    var hukidashi1 = Instantiate(hukidashiObj, new Vector2(orderPos[i], 3.3f), Quaternion.identity);
-                    hukidashi1.transform.localScale = new Vector2(1.25f, 1.25f);
-                    hukidashi1.transform.parent = menuObject.transform;
-                    var yakitori = Instantiate(yakitoriObj, new Vector2(orderPos[i], 3.3f), Quaternion.identity);
+                    var yakitori = Instantiate(yakitoriObj, new Vector2(OrderPos[i], 3.6f), Quaternion.identity);
+                    yakitori.transform.localScale = new Vector2(0.8f, 0.8f);
                     yakitori.transform.parent = menuObject.transform;
-                    yield return new WaitForSeconds(2.0f);
-                    Delete();
-                    yield return new WaitForSeconds(1.0f);
                     break;
+                //酒を表示
                 case 1:
-                    var hukidashi2 = Instantiate(hukidashiObj, new Vector2(orderPos[i], 3.3f), Quaternion.identity);
-                    hukidashi2.transform.localScale = new Vector2(1.25f, 1.25f);
-                    hukidashi2.transform.parent = menuObject.transform;
-                    var sake = Instantiate(sakeObj, new Vector2(orderPos[i], 3.3f), Quaternion.identity);
-                    sake.transform.localScale = new Vector2(0.35f, 0.35f);
+                    var sake = Instantiate(sakeObj, new Vector2(OrderPos[i], 3.6f), Quaternion.identity);
+                    sake.transform.localScale = new Vector2(0.3f, 0.3f);
                     sake.transform.parent = menuObject.transform;
-                    yield return new WaitForSeconds(2.0f);
-                    Delete();
-                    yield return new WaitForSeconds(1.0f);
                     break;
+                //サラダを表示
                 case 2:
-                    var hukidashi3 = Instantiate(hukidashiObj, new Vector2(orderPos[i], 3.3f), Quaternion.identity);
-                    hukidashi3.transform.localScale = new Vector2(1.25f, 1.25f);
-                    hukidashi3.transform.parent = menuObject.transform;
-                    var salad = Instantiate(saladObj, new Vector2(orderPos[i], 3.3f), Quaternion.identity);
-                    salad.transform.localScale = new Vector2(0.45f, 0.45f);
+                    var salad = Instantiate(saladObj, new Vector2(OrderPos[i], 3.6f), Quaternion.identity);
+                    salad.transform.localScale = new Vector2(0.35f, 0.35f);
                     salad.transform.parent = menuObject.transform;
-                    yield return new WaitForSeconds(2.0f);
-                    Delete();
-                    yield return new WaitForSeconds(1.0f);
                     break;
+                //刺身を表示
                 default:
-                    var hukidashi4 = Instantiate(hukidashiObj, new Vector2(orderPos[i], 3.3f), Quaternion.identity);
-                    hukidashi4.transform.localScale = new Vector2(1.25f, 1.25f);
-                    hukidashi4.transform.parent = menuObject.transform;
-                    var sashimi = Instantiate(sashimiObj, new Vector2(orderPos[i], 3.3f), Quaternion.identity);
-                    sashimi.transform.localScale = new Vector2(0.35f, 0.35f);
+                    var sashimi = Instantiate(sashimiObj, new Vector2(OrderPos[i], 3.6f), Quaternion.identity);
+                    sashimi.transform.localScale = new Vector2(0.3f, 0.3f);
                     sashimi.transform.parent = menuObject.transform;
-                    yield return new WaitForSeconds(2.0f);
-                    Delete();
-                    yield return new WaitForSeconds(1.0f);
                     break;
             }
+            //2秒後に表示された吹き出しと商品を消す
+            yield return new WaitForSeconds(2.0f);
+            Delete();
+            OrderCounterOFF();
         }
-
-        //正解を表示
-        OrderAnswer();
+        yield return new WaitForSeconds(1.0f);
+        ButtonController button = GetComponent<ButtonController>();
+        button.DrinkSceneButtonON();
+        NumCounter = 0;
 
     }
   
-    //コルーチン処理の実行
     public void Order()
     {
         StartCoroutine("OrderMethod");
     }
 
-    //注文の表示を消す
     public void Delete()
     {
         var Delete = menuObject.transform;
@@ -123,52 +140,94 @@ public class DrinkScene : MonoBehaviour {
         }
     }
 
-    //注文の答えを表示
+    //注文の答えの表示
     public void OrderAnswer()
     {
-        for (int i = 0; i < foodsBox.Length; i++)
+        OrderCounterON();
+
+        for (int i = 0; i < OrderBox.Length; i++)
         {
-            switch (foodsBox[i])
+            //吹き出しを表示
+            var hukidashi = Instantiate(hukidashiObj, new Vector2(OrderPos[i], 3.3f), Quaternion.identity);
+            hukidashi.transform.localScale = new Vector2(1.25f, 1.25f);
+            hukidashi.transform.parent = menuObject.transform;
+            switch (OrderBox[i])
             {
+                //やきとりを表示
                 case 0:
-                    var hukidashi1 = Instantiate(hukidashiObj, new Vector2(orderPos[i], 3.3f), Quaternion.identity);
-                    hukidashi1.transform.localScale = new Vector2(1.25f, 1.25f);
-                    hukidashi1.transform.parent = menuObject.transform;
-                    var yakitori = Instantiate(yakitoriObj, new Vector2(orderPos[i], 3.3f), Quaternion.identity);
+                    var yakitori = Instantiate(yakitoriObj, new Vector2(OrderPos[i], 3.6f), Quaternion.identity);
+                    yakitori.transform.localScale = new Vector2(0.8f, 0.8f);
                     yakitori.transform.parent = menuObject.transform;
                     break;
+                //酒を表示
                 case 1:
-                    var hukidashi2 = Instantiate(hukidashiObj, new Vector2(orderPos[i], 3.3f), Quaternion.identity);
-                    hukidashi2.transform.localScale = new Vector2(1.25f, 1.25f);
-                    hukidashi2.transform.parent = menuObject.transform;
-                    var sake = Instantiate(sakeObj, new Vector2(orderPos[i], 3.3f), Quaternion.identity);
-                    sake.transform.localScale = new Vector2(0.35f, 0.35f);
+                    var sake = Instantiate(sakeObj, new Vector2(OrderPos[i], 3.6f), Quaternion.identity);
+                    sake.transform.localScale = new Vector2(0.3f, 0.3f);
                     sake.transform.parent = menuObject.transform;
                     break;
+                //サラダを表示
                 case 2:
-                    var hukidashi3 = Instantiate(hukidashiObj, new Vector2(orderPos[i], 3.3f), Quaternion.identity);
-                    hukidashi3.transform.localScale = new Vector2(1.25f, 1.25f);
-                    hukidashi3.transform.parent = menuObject.transform;
-                    var salad = Instantiate(saladObj, new Vector2(orderPos[i], 3.3f), Quaternion.identity);
-                    salad.transform.localScale = new Vector2(0.45f, 0.45f);
+                    var salad = Instantiate(saladObj, new Vector2(OrderPos[i], 3.6f), Quaternion.identity);
+                    salad.transform.localScale = new Vector2(0.35f, 0.35f);
                     salad.transform.parent = menuObject.transform;
                     break;
+                //刺身を表示
                 default:
-                    var hukidashi4 = Instantiate(hukidashiObj, new Vector2(orderPos[i], 3.3f), Quaternion.identity);
-                    hukidashi4.transform.localScale = new Vector2(1.25f, 1.25f);
-                    hukidashi4.transform.parent = menuObject.transform;
-                    var sashimi = Instantiate(sashimiObj, new Vector2(orderPos[i], 3.3f), Quaternion.identity);
-                    sashimi.transform.localScale = new Vector2(0.35f, 0.35f);
+                    var sashimi = Instantiate(sashimiObj, new Vector2(OrderPos[i], 3.6f), Quaternion.identity);
+                    sashimi.transform.localScale = new Vector2(0.3f, 0.3f);
                     sashimi.transform.parent = menuObject.transform;
                     break;
             }
         }
     }
+    
+    public void OrderNum()
+    {
+        switch (Num[NumCounter])
+        {
+            case 0:
+                OrderCounter1.gameObject.SetActive(true);
+                OrderCounter1.text = "× " + OrderCounter[NumCounter].ToString();
+                break;
+            case 1:
+                OrderCounter2.gameObject.SetActive(true);
+                OrderCounter2.text = "× " + OrderCounter[NumCounter].ToString();
+                break;
+            case 2:
+                OrderCounter3.gameObject.SetActive(true);
+                OrderCounter3.text = "× " + OrderCounter[NumCounter].ToString();
+                break;
+            default:
+                OrderCounter4.gameObject.SetActive(true);
+                OrderCounter4.text = "× " + OrderCounter[NumCounter].ToString();
+                break;
+        }
+        NumCounter++;
+    }
+
+    public void OrderCounterON()
+    {
+        OrderCounter1.gameObject.SetActive(true);
+        OrderCounter2.gameObject.SetActive(true);
+        OrderCounter3.gameObject.SetActive(true);
+        OrderCounter4.gameObject.SetActive(true);
+    }
+
+    public void OrderCounterOFF()
+    {
+        OrderCounter1.gameObject.SetActive(false);
+        OrderCounter2.gameObject.SetActive(false);
+        OrderCounter3.gameObject.SetActive(false);
+        OrderCounter4.gameObject.SetActive(false);
+    }
+
 
 	void Start () {
         OrderShuffle();
+        PosShuffle();
         Order();
-	}
+        OrderCounterOFF();
+    }
 	
 	void Update () {
 		
