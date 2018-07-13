b6db328c1e0706c84bee1613902db33a6e8d057b
paseso <16d3170001ae@edu.tech.ac.jp>
2018-07-13 19:17:24 +0900

    Hacking 名刺ごと取得する処理追加　未完成

---
+++ Syatiku/Assets/Scripts/MiniGame/Hacking/HackMeishi.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class HackMeishi : MonoBehaviour {
+
+    [SerializeField, Tooltip("PaperPrefab")]
+    private GameObject paper_prefab;
+    private RectTransform Meishi_rect;
+
+    private GameObject GetWord;
+    [HideInInspector]
+    public bool _document = false;
+
+    // Use this for initialization
+    void Start () {
+        GetWord = GameObject.Find("Canvas/Check/GetWord");
+        Meishi_rect = GameObject.Find("Canvas/Zoom/Meishi/Image").GetComponent<RectTransform>();
+        _document = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MeishiPrefab()
+    {
+        Debug.Log("名刺");
+        Meishi_rect.GetComponent<Image>().color = new Color(255,255,255,255);
+        Meishi_rect.transform.localPosition = new Vector2(311,187);
+    }
+
+    /// <summary>
+    /// 名刺タップの処理
+    /// </summary>
+    public void MeishiTap()
+    {
+        if (Meishi_rect.transform.childCount == 2)
+            return;
+        GameObject _get_doc = Instantiate(paper_prefab, GetWord.transform);
+        _get_doc.transform.SetAsLastSibling();
+        GameObject obj = new GameObject();
+        obj.transform.SetParent(Meishi_rect.transform, false);
+        _document = true;
+
+        Sequence s = DOTween.Sequence();
+        s.Append(Meishi_rect.DOLocalMove(new Vector3(374, 221, 0), 0.7f))
+            .Join(Meishi_rect.DOScale(0.2f, 0.7f))
+            .OnComplete(() => Meishi_rect.gameObject.SetActive(false));
+    }
+}
