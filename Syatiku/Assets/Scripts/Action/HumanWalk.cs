ad93b5d0c05755d1eeba235a473c9e5ad9103079
a <a>
2018-07-13 13:41:56 +0900

    ミニゲーム遷移ボタンの判定を大きく

---
+++ Syatiku/Assets/Scripts/Action/HumanWalk.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanWalk : MonoBehaviour {
+    // 人間画像
+    [SerializeField]
+    private Sprite idle, walk;
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
