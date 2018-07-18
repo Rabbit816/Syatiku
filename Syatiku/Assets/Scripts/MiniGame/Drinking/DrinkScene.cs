d99609da6b27932459afdff197118b1a29874c69
Hara Kazunori <17c1020021hk@edu.tech.ac.jp>
2018-07-18 16:34:03 +0900

    飲み会シーンのスクリプト修正

--- Syatiku/Assets/Scripts/MiniGame/Drinking/DrinkScene.cs
+++ Syatiku/Assets/Scripts/MiniGame/Drinking/DrinkScene.cs
@@ -28,12 +28,15 @@ public class DrinkScene : MonoBehaviour {
     //注文を表示するための配列
     private int[] OrderBox = new int[4];
     private int[] OrderCounter = new int[4];
-    private int[] OrderPos = new int[4] {-6, -2, 2, 6};
+    private float[] OrderPos = new float[4] {-6, -2, 2, 6};
 
     private int[] Num = new int[4];
     private int NumCounter = 0;
 
-    
+    //もとのOrderPosの中身の３番目までを保存しておく変数
+    private float OriginPos1;
+    private float OriginPos2;
+    private float OriginPos3;
 
 
     //注文の配列の用意
@@ -62,15 +65,15 @@ public class DrinkScene : MonoBehaviour {
         //Num配列の用意
         for (int i = 0; i < this.OrderPos.Length; i++)
         {
-            if(this.OrderPos[i] == -6)
+            if(this.OrderPos[i] == OriginPos1)
             {
                 Num[i] = 0;
             }
-            else if(this.OrderPos[i] == -2)
+            else if(this.OrderPos[i] == OriginPos2)
             {
                 Num[i] = 1;
             }
-            else if(this.OrderPos[i] == 2)
+            else if(this.OrderPos[i] == OriginPos3)
             {
                 Num[i] = 2;
             }
@@ -326,6 +329,7 @@ public class DrinkScene : MonoBehaviour {
     }
 
 	void Start () {
+        //ゲームの初期状態を用意する処理
         button = GetComponent<ButtonController>();
         denmoku = GetComponent<Denmoku>();
         merter = GetComponent<Merter>();
@@ -339,15 +343,17 @@ public class DrinkScene : MonoBehaviour {
         this.Answer2 = GameObject.Find("OrderAnswer/Answer2");
         this.Answer3 = GameObject.Find("OrderAnswer/Answer3");
         this.Answer4 = GameObject.Find("OrderAnswer/Answer4");
-
-        this.OrderShuffle();
-        this.PosShuffle();
-        this.Order();
+        this.OriginPos1 = this.OrderPos[0];
+        this.OriginPos2 = this.OrderPos[1];
+        this.OriginPos3 = this.OrderPos[2];
         this.OrderCounterOFF();
         this.AnswerResultOFF();
         button.NextGame.gameObject.SetActive(false);
-        
 
+        //ゲーム開始に必要な処理
+        this.OrderShuffle();
+        this.PosShuffle();
+        this.Order();
     }
 	void Update () {
 
