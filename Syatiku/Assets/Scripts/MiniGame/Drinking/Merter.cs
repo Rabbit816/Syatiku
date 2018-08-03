b2c1cc16e44821e8bcdd18c4e9279d031e3d040c
Maker913 <17c1020010ks@edu.tech.ac.jp>
2018-08-03 14:47:33 +0900

    修正
    メーターが0になったときミリでゲージが見えてしまう点を修正

--- Syatiku/Assets/Scripts/MiniGame/Drinking/Merter.cs
+++ Syatiku/Assets/Scripts/MiniGame/Drinking/Merter.cs
@@ -5,41 +5,45 @@ using UnityEngine.UI;
 using System;
 public class Merter : MonoBehaviour {
     public void Awake()
-     {
+    {
          rt = GameObject.Find("InfoMeter").gameObject.GetComponent<RectTransform>();
          maxValue = rt.sizeDelta.x;
     }
-
     public void UpdateValue (float t)
-     {
-         float x = Mathf.Lerp(0f, maxValue, t);
+    {
+         float x = Mathf.Lerp(0.00f, maxValue, t);
          rt.sizeDelta = new Vector2(x,rt.sizeDelta.y);
     }
+    /// <summary>
+    /// メーターの減少及びシーン移動
+    /// </summary>
     public void Update()
     {
         if (MeterON == true)
         {
+            InfoMeter.SetActive(true);
             UpdateValue(value);
-            value -= 0.002f;
-            if (value < 0 || flg == true)
+            value -= 0.001f;
+            if (value < 0.00f || flg == true)
             {
                 Common.Instance.ChangeScene(Common.SceneName.Result);
-                if (value < 0)
+                if (value < 0.00f)
                 {
+                    InfoMeter.SetActive(false);
                     flg = true;
                     MeterON = false;
                 }
             }
-        }
-        
+        } 
     }
-
-
-
     public void Moving()
     {
         flg = false;
     }
+    /// <summary>
+    /// 関数宣言
+    /// </summary>
+    public GameObject InfoMeter;
     private bool flg;
     public float value = 1f;
     private float maxValue;
