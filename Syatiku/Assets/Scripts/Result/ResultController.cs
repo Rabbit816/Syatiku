e48263582a4328154e03b0854aa3fc8489690613
DESKTOP-2V30706\Yuki Washizu <himazin19970630@gmail.com>
2018-08-17 11:41:51 +0900

    リザルトの修正、喫煙の差し替え

--- Syatiku/Assets/Scripts/Result/ResultController.cs
+++ Syatiku/Assets/Scripts/Result/ResultController.cs
@@ -5,21 +5,32 @@ using UnityEngine.UI;
 
 public class ResultController : MonoBehaviour {
     
-    private string[] scoreText = new string[2];
-
     [SerializeField]
     private GameObject canvas;
 
     [SerializeField]
+    private Image enemy, spot;
+
+    [SerializeField]
+    private Button backAction;
+
+    [SerializeField]
+    private Sprite _enemySprite;
+
+    [SerializeField]
     private RectTransform[] proPos = new RectTransform[2];
 
+    private string[] scoreText = new string[2];
+
     private GameObject[] property = new GameObject[2];
+
+    private bool successFlag = false;
 	// ミニゲームで獲得した情報を表示
 	void Start () {
         // ミニゲーム分岐
-        if (Common.Instance.clearFlag[Common.Instance.miniNum])
-            switch (Common.Instance.miniNum)
-            {
+        if (Common.Instance.clearFlag[Common.Instance.miniNum]) {
+            successFlag = true;
+            switch (Common.Instance.miniNum) {
                 case 0:
                     scoreText[0] = "情報A";
                     scoreText[1] = "情報B";
@@ -32,8 +43,8 @@ public class ResultController : MonoBehaviour {
                     scoreText[0] = "情報E";
                     break;
             }
-        else
-        {
+        } else {
+            successFlag = false;
             scoreText[0] = "スカ";
             scoreText[1] = "スカ";
         }
@@ -41,14 +52,27 @@ public class ResultController : MonoBehaviour {
         StartCoroutine(CreateProperty());
     }
 
-    public void TitleBack()
+    /// <summary>
+    /// 行動選択に戻る
+    /// </summary>
+    public void ActionBack()
     {
         Common.Instance.actionCount--;
         Common.Instance.ChangeScene(Common.SceneName.Action);
     }
 
+    /// <summary>
+    /// 獲得した情報を表示する
+    /// </summary>
+    /// <returns></returns>
     public IEnumerator CreateProperty()
     {
+        // EnemyとSpotを表示
+        yield return new WaitForSeconds(0.5f);
+        enemy.gameObject.SetActive(true);
+        spot.gameObject.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+
         // prefabから情報フキダシを生成
         for (int i = 0; i < property.Length; i++)
         {
@@ -58,6 +82,13 @@ public class ResultController : MonoBehaviour {
             property[i].transform.GetChild(0).GetComponent<Text>().text = scoreText[i];
             yield return new WaitForSeconds(0.5f);
         }
+        yield return new WaitForSeconds(1f);
+        if(successFlag) enemy.sprite = _enemySprite;
+        else {
+
+        }
+        
+        backAction.gameObject.SetActive(true);
     }
     
 }
